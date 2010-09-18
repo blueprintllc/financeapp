7d1c97459eab22bacde9a36e45fc72ce318d1dce
P-DaWg <pavitm@gmail.com>
2010-09-18 05:30:01 -0400

    tklsjef

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
 
 	has_many :ideas
-    acts_as_authentic do |c|
-      c.login_field = :login # for available options see documentation in: Authlogic::ActsAsAuthentic
-    end 
+    acts_as_authentic
+
     	
 end
